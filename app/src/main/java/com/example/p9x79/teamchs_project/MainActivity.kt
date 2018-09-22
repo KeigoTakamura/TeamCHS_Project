aa22cc2ac391502c1bea9a7b4c2b8165526dc93b
P9X79 <s15528@salesio-sp.ac.jp>
2018-09-22 16:36:14 +0900

    テスト!!!

---
+++ app/src/main/java/com/example/p9x79/teamchs_project/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.p9x79.teamchs_project
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
