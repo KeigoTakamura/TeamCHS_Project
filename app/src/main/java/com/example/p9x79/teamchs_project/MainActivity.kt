9d9019c74731d5f086a5358f54d94fbc19c61951
P9X79 <s15528@salesio-sp.ac.jp>
2018-09-22 21:27:57 +0900

    テスト!!!

--- app/src/main/java/com/example/p9x79/teamchs_project/MainActivity.kt
+++ app/src/main/java/com/example/p9x79/teamchs_project/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.p9x79.teamchs_project
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tap_here.setOnClickListener {
+            textView.text = "ボタンがタップされた"
+        }
     }
 }
