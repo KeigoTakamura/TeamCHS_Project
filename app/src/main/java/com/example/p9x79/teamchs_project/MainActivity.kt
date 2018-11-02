4dcda88c4cacaa0706c68fd24caaed9402119321
takamura keigo <s15528@salesio-sp.ac.jp>
2018-11-03 07:18:46 +0900

    キャンセルメッセージ表示

--- app/src/main/java/com/example/p9x79/teamchs_project/MainActivity.kt
+++ app/src/main/java/com/example/p9x79/teamchs_project/MainActivity.kt
@@ -93,6 +93,7 @@ class MainActivity : AppCompatActivity()
 
         cancelAlarm.setOnClickListener {
             cancelAlarmManager()
+            toast("アラームをキャンセルしました")
         }
 
         dateText.setOnClickListener {
